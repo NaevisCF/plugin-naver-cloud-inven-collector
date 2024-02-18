df61ed1a4bd044d9138f7bdcf33cb6cb9ed61ad0
Jihyung Song <jihyungsong@Jihyungui-MacBookAir-2.local>
2024-02-18 15:38:08 +0900

    update setup

--- src/setup.py
+++ src/setup.py
@@ -35,6 +35,6 @@ setup(
         'spaceone-api',
         'spaceone-inventory'
     ],
-    package_data={"inventory": ["metadata/spaceone/*.yaml"]},
+    package_data={"inventory": ["metadata/spaceone/*/*.yaml"]},
     zip_safe=False,
 )
