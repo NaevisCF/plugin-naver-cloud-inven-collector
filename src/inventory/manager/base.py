8c119cae615075ee5536c2fc4d2c50fd43f7263b
kiku99 <elsd0326@gmail.com>
2024-06-07 15:26:51 +0900

    refactor: manager 구조 변경
    resourcemanager factory 생성

--- src/inventory/manager/base.py
+++ src/inventory/manager/base.py
@@ -1,6 +1,9 @@
 from spaceone.core.manager import BaseManager
+from spaceone.inventory.plugin.collector.lib import *
 import importlib
 import logging
+from typing import List
+import abc
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -29,6 +32,42 @@ class ResourceManager(BaseManager):
         manager_instance = self._get_manager_instance(CLOUD_SERVICE_MANAGER_MAP[service])
         return manager_instance.collect_resources(options, secret_data)
 
+    def collect_resources(self, options, secret_data):
+        try:
+            yield from self.collect_cloud_service_type(options, secret_data)
+            yield from self.collect_cloud_service(options, secret_data)
+        except Exception as e:
+            yield make_error_response(
+                error=e,
+                provider=self.provider,
+                cloud_service_group=self.cloud_service_group,
+                cloud_service_type=self.cloud_service_type
+            )
+
+    def collect_cloud_service_type(self):
+        cloud_service_types = self.create_cloud_service_type()
+        for cloud_service_type in cloud_service_types:
+            yield make_response(
+                cloud_service_type=cloud_service_type,
+                match_keys=[["name", "group", "provider"]],
+                resource_type="inventory.CloudServiceType"
+            )
+
+    def collect_cloud_service(self, option: dict, secret_data: dict) -> List[dict]:
+        cloud_services = self.create_cloud_service(option, secret_data)
+        for cloud_service in cloud_services:
+            yield make_response(
+                cloud_service=cloud_service,
+                match_keys=[[
+                    "reference.resource_id",
+                    "provider",
+                    "cloud_service_type",
+                    "cloud_service_group",
+                    "account",
+                ]],
+                resource_type="inventory.CloudService"
+            )
+
     @staticmethod
     def get_service_names():
         service_names = []
@@ -48,3 +87,13 @@ class ResourceManager(BaseManager):
             "resource_id": resource_id,
             "external_link": link
         }
+
+    @abc.abstractmethod
+    def create_cloud_service_type(self):
+        raise NotImplementedError(
+            "method `create_cloud_service_type` should be implemented"
+        )
+
+    @abc.abstractmethod
+    def create_cloud_service(self, region, options, secret_data, schema):
+        raise NotImplementedError("method `create_cloud_service` should be implemented")
